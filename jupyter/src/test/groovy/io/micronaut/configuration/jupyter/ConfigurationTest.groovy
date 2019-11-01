c29d993864088bdb97aeed77797736d90cbc1b89
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-10-31 18:44:48 -0700

    Remove unused variables from ConfigurationTest.groovy

--- jupyter/src/test/groovy/io/micronaut/configuration/jupyter/ConfigurationTest.groovy
+++ jupyter/src/test/groovy/io/micronaut/configuration/jupyter/ConfigurationTest.groovy
@@ -6,12 +6,6 @@ import spock.lang.Specification
 
 class ConfigurationTest extends Specification {
 
-    def defaultKernelDirectory = "micronaut"
-    def defaultKernels = "/usr/local/share/jupyter/kernels"
-    def kernelJsonName = "kernel.json"
-    def defaultKernelLocation = "$defaultKernels/$defaultKernelDirectory/$kernelJsonName"
-    def serverUrl = "http://localhost:8080"
-
     def "can be disabled"() {
         given:
         // create application context
