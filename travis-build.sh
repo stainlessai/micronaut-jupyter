adcdb2cf21267534d89adc2167b89699ab94f75e
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-11-01 18:16:56 -0700

    Match version string in branch instead of tag to decide whether to publish in travis-build.sh (MJ-14)

--- travis-build.sh
+++ travis-build.sh
@@ -5,10 +5,9 @@ EXIT_STATUS=0
 ./gradlew jupyter:assemble
 
 echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
-echo "TRAVIS_TAG=$TRAVIS_TAG"
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
-if [[ $TRAVIS_EVENT_TYPE == 'api' && $TRAVIS_TAG =~ ^v && $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ $TRAVIS_EVENT_TYPE == 'api' && $TRAVIS_BRANCH =~ ^v[0-9]\.[0-9]\.[0-9] && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   echo "Publishing artifacts to maven central repo."
   ./gradlew publishJupyterConfigurationLibraryPublicationToMavenCentralRepository --stacktrace
 else
