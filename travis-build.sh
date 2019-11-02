ec5ce53468a960f54030c4fda94a7dcb0a55e460
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-11-01 18:16:13 -0700

    Only run tests if not publishing in travis-build.sh (MJ-14)

--- travis-build.sh
+++ travis-build.sh
@@ -2,7 +2,6 @@
 
 EXIT_STATUS=0
 
-./gradlew test
 ./gradlew jupyter:assemble
 
 echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
@@ -10,7 +9,11 @@ echo "TRAVIS_TAG=$TRAVIS_TAG"
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 if [[ $TRAVIS_EVENT_TYPE == 'api' && $TRAVIS_TAG =~ ^v && $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+  echo "Publishing artifacts to maven central repo."
   ./gradlew publishJupyterConfigurationLibraryPublicationToMavenCentralRepository --stacktrace
+else
+  echo "Running tests."
+  ./gradlew test
 fi
 
 exit $EXIT_STATUS
