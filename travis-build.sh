0caf2d2c69c9156bde0dba2475755552c78e549a
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-11-01 17:58:11 -0700

    Configure publishing to maven central repos in gradle and travis ci files (MJ-14)

--- travis-build.sh
+++ travis-build.sh
@@ -5,4 +5,12 @@ EXIT_STATUS=0
 ./gradlew test
 ./gradlew jupyter:assemble
 
+echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
+echo "TRAVIS_TAG=$TRAVIS_TAG"
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
+echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+if [[ $TRAVIS_EVENT_TYPE == 'api' && $TRAVIS_TAG =~ ^v && $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+  ./gradlew publishJupyterConfigurationLibraryPublicationToMavenCentralRepository --stacktrace
+fi
+
 exit $EXIT_STATUS
