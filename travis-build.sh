0315be992b3ad4ffba3c22389f0cd9639788fd4e
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-11-01 18:39:03 -0700

    Only publish for single language build in travis-build.sh (MJ-14)

--- travis-build.sh
+++ travis-build.sh
@@ -7,9 +7,14 @@ EXIT_STATUS=0
 echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+echo "TRAVIS_JDK_VERSION=$TRAVIS_JDK_VERSION"
 if [[ $TRAVIS_EVENT_TYPE == 'api' && $TRAVIS_BRANCH =~ ^v[0-9]\.[0-9]\.[0-9] && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   echo "Publishing artifacts to maven central repo."
-  ./gradlew publishJupyterConfigurationLibraryPublicationToMavenCentralRepository --stacktrace
+  if [ "${TRAVIS_JDK_VERSION}" == "openjdk11" ] ; then
+    ./gradlew publishJupyterConfigurationLibraryPublicationToMavenCentralRepository --stacktrace
+  else
+    echo "Skipping publish for JDK version $TRAVIS_JDK_VERSION"
+  fi
 else
   echo "Running tests."
   ./gradlew test
