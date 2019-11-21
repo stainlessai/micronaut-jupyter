c33c1542fb033d114de8862f58dec737c0798469
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-11-20 18:48:40 -0800

    Add doc for md test FriendRepository (MJ-7)

--- jupyter/src/test/groovy/ai/stainless/micronaut/jupyter/md/FriendRepository.groovy
+++ jupyter/src/test/groovy/ai/stainless/micronaut/jupyter/md/FriendRepository.groovy
@@ -6,6 +6,10 @@ import io.micronaut.data.repository.CrudRepository
 
 @Repository
 interface FriendRepository extends CrudRepository<Person, Long> {
+    /*
+     * Micronaut doesn't support lazy initialization, so we need to specify a
+     * join for our query.
+     */
     @EntityGraph(attributePaths = ["friends"])
     Person find (String firstName, String lastName)
 }
