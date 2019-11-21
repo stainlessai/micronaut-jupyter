75ebf191d0fd59ae522586f73b97ddaab6bf2a5c
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-11-21 12:05:00 -0800

    Automate publishing release on travis (travis manual builds no longer working)

--- travis-build.sh
+++ travis-build.sh
@@ -4,11 +4,10 @@ EXIT_STATUS=0
 
 ./gradlew jupyter:assemble
 
-echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 echo "TRAVIS_JDK_VERSION=$TRAVIS_JDK_VERSION"
-if [[ $TRAVIS_EVENT_TYPE == 'api' && $TRAVIS_BRANCH =~ ^v[0-9]\.[0-9]\.[0-9] && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ $TRAVIS_BRANCH =~ ^v[0-9]\.[0-9]\.[0-9] && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   echo "Publishing artifacts to maven central repo."
   if [ "${TRAVIS_JDK_VERSION}" == "openjdk11" ] ; then
     ./gradlew publishJupyterConfigurationLibraryPublicationToMavenCentralRepository --stacktrace
