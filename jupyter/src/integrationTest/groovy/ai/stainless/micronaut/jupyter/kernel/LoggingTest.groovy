6e2338f33b8502dd207318e682f73441f3b42509
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-12-06 15:11:16 -0800

    Write test for jupyter.kernel.redirectLogOutput property (MJ-17 #6)

--- jupyter/src/integrationTest/groovy/ai/stainless/micronaut/jupyter/kernel/LoggingTest.groovy
+++ jupyter/src/integrationTest/groovy/ai/stainless/micronaut/jupyter/kernel/LoggingTest.groovy
@@ -1,12 +1,12 @@
 package ai.stainless.micronaut.jupyter.kernel
 
+import io.micronaut.context.annotation.Property
 import io.micronaut.test.annotation.MicronautTest
 
 @MicronautTest
 class LoggingTest extends KernelSpec {
 
     def "Logging output doesn't go to cell output" () {
-
         when:
         // execute notebook
         NotebookExecResult notebookResult = executeNotebook("logging")
@@ -27,7 +27,21 @@ class LoggingTest extends KernelSpec {
             "Before in-cell logging\n",
             "After in-cell logging\n"
         ]
+    }
 
+    @Property(name = "jupyter.kernel.redirectLogOutput", value = "")
+    def "Logging redirection can be disabled" () {
+        when:
+        // execute notebook
+        NotebookExecResult notebookResult = executeNotebook("logging")
+
+        then:
+        // commands should have executed successfully
+        verifyExecution(notebookResult)
+        // test stdout of cell
+        notebookResult.outJson.cells?.get(2)?.outputs?.find { it.name == "stdout" }?.text?.size() == 5
+        notebookResult.outJson.cells?.get(4)?.outputs?.find { it.name == "stdout" }?.text?.get(1)?.contains("Info line")
+        notebookResult.outJson.cells?.get(5)?.outputs?.find { it.name == "stdout" }?.text?.get(3)?.contains("Error line")
     }
 
 }
