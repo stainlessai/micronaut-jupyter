cb18fd593665dc2746547aba826ce018e09d0794
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2020-02-19 22:45:01 -0800

    Configure travis-ci to build docs and deploy them to GitHub Pages (MJ-28 #26)

---
+++ travis/prepare-docs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo "Preparing docs for deployment..."
+echo ""
+
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
+echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+
+# do not deploy docs on pull request
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Pull request detected, skipping docs preparation"
+    exit 1
+fi
+
+# Generate docs
+./gradlew docs
+BUILD_DOCS="build/docs"
+
+# if this is a version tag
+if [[ "$TRAVIS_BRANCH" =~ ^v[0-9]\.[0-9]\.[0-9] ]]; then
+    # parse version info
+    VERSION="$TRAVIS_BRANCH"            # v1.2.3
+    VERSION=${VERSION:1}                # 1.2.3
+    MAJOR_VERSION=${VERSION:0:4}        # 1.2.
+    MAJOR_VERSION="${MAJOR_VERSION}x"   # 1.2.x
+    
+    # create version directories
+    echo "Creating docs directory: $VERSION"
+    rm -rf "$VERSION"
+    mv "$BUILD_DOCS" "$VERSION"
+    
+    echo "Creating docs directory: $MAJOR_VERSION"
+    rm -rf "$MAJOR_VERSION"
+    cp -r "$VERSION" "$MAJOR_VERSION"
+else
+    # copy docs to directory for branch
+    echo "Creating docs directory: $TRAVIS_BRANCH"
+    rm -rf "$TRAVIS_BRANCH"
+    mv "$BUILD_DOCS" "$TRAVIS_BRANCH"
+
+    # if this is the master branch
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        # create latest alias
+        echo "Creating docs directory: latest"
+        rm -rf latest
+        cp -r "$TRAVIS_BRANCH" latest
+    fi
+fi
